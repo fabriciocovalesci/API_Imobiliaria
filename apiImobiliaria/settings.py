2b97efe7f89360d4fa62a1f69a0116411616325a
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-10-28 16:10:11 -0300

    ajuste no MEDIA_ROOT

--- apiImobiliaria/settings.py
+++ apiImobiliaria/settings.py
@@ -87,10 +87,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apiImobiliaria.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+# ajustar dataBase para postgres
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -118,9 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
@@ -132,12 +126,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/home/fabricio/Documentos/APNPs/Projeto_Integrador/API'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
